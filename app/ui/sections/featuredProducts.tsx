22c5f11a19a994242c01fa28b7a29d8c24f35d8e
Oscar Luntha <lunthaoscar@gmail.com>
2025-06-02 22:42:20 +0200

    Create and style the section for holding company prodcts up for sell

---
+++ app/ui/sections/featuredProducts.tsx
@@ -0,0 +1,8 @@
+export default function FeaturedProducts(){
+    return (
+        <section className="px-6 py-12 text-center bg-white">
+            <h1 className="text-4xl md:text-5xl font-bold mb-10 text-gray-900">Featured products</h1>
+            <p>Get free services when you buy an electronic product from us</p>
+        </section>
+    )
+}
